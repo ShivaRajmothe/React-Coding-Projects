ca970f2d92f4f5b9e550417a88ed71c026cead93
ShivaRajmothe <65208192+ShivaRajmothe@users.noreply.github.com>
2026-02-12 06:37:12 +0000

    initial commit

---
+++ Maching-coding/src/Accordion.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+import { FaChevronDown, FaChevronUp } from "react-icons/fa"; 
+import './styles.css'
+
+function Accordion({ items }) {
+    return (
+        <div>
+        </div>
+    );
+}
+
+export default Accordion;
