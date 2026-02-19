ed97277bac05bfae8ddcdfca83af60261ff9c280
ShivaRajmothe <65208192+ShivaRajmothe@users.noreply.github.com>
2026-02-19 12:56:30 +0000

    imports update

--- Maching-coding/src/main.jsx
+++ Maching-coding/src/main.jsx
@@ -2,11 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import Accordion from './components/Accordion.js'
-import Counter from './components/Counter.jsx'
-import UsersList from './components/UsersList.js'
-import Products from './components/Products.js'
-import TodoList from './components/TodoList.js'
+import Accordion from '../src/components/Accordion.jsx'
+import Counter from '../src/components/Counter.jsx'
+import UsersList from '../src/components/UsersList.jsx'
+import Products from '../src/components/Products.jsx'
+import TodoList from '../src//components/TodoList.jsx'
 createRoot(document.getElementById('root')).render(
   <>
     {/* <App />  */}
